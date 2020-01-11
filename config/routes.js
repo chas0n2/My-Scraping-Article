4c4d03341e2e01b77eb4e5a2dc12035eaec53417
chason <chasonlsmith@gmail.com>
2020-01-10 20:06:39 -0700

    updated index.js added function for empty card and create articles cards

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,9 @@
+const scraper = require("../scripts/articlescrape");
+
+const headlinesCon = require("../controllers/headlines");
+const notesCon = require("../controllers/note");
+
+
 module.exports = function(router) {
     //Homepage
     router.get("/", function(req, res) {
@@ -7,4 +13,20 @@ module.exports = function(router) {
     router.get("/saved", function(req, res) {
         res.render("saved");
     });
+    router.get("/api/fetch", function(req, res) {
+        headlinesCon.fetch(function(err, docs) {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "There are no new articles available today, check back tomorrow!"
+                });
+            }
+            else {
+                res.json({
+                    message: "New Articles " + docs.insertedCount + " Have been added!"
+                });
+            }
+        });
+    });
+
 }
+
---
+++ public/assets/JS/index.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+
+    const articleContainer = $(".article-container");
+    $(document).on("click", ".btn.save", handleArticleSave);
+    $(document).on("click", ".scrape-new", handleArticleScrape);
+
+    initPage();
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=false")
+        .then(function(data) {
+            if (data && data.length) {
+                renderArticles(data);
+            }
+            else {
+                renderEmpty();
+            }
+        });
+    }
+
+    function renderArticles(articles) {
+
+    const articleCards = [];
+    for (let i = 0; i < articles.length; i++) {
+        articleCards.push(createCards(articles[i]));
+        
+    }
+    articleContainer.append(articleCards);
+    }
+    function createCards(article) {
+
+        const card =
+        $(["<div class='card'>",
+        "<div class='card-header text-center'>",
+        "<h3>",
+        article.headline,
+        "<a class='btn btn-success save'>",
+        "Save Article",
+        "</a>",
+        "</h3>",
+        "</div>",
+        "<div class='card-body text-center'>",
+        "<h4>",
+        article.summary,
+        "</h4>",
+        "</div>",
+        "</div>"
+    ].join(""));
+    card.data("_id", article._id);
+    return card;
+  }
+
+  function renderEmpty() {
+      const emptyAlert = 
+      $(["<div class='card'>",
+      "<div class='card-header text-center'>",
+      "<h3>No saved articles.</h3>",
+      "</div>",
+      "<div class='card-body text-center'>",
+      "<h4>Use the 'Scrape New Articles' button to get started!</h4>",
+      "</div>",
+      "</div>"
+        ].join(""));
+        articleContainer.append(emptyAlert);
+  }
+
+  function handleArticleSave() {
+      const articleToSave = $(this).parents(".card").data();
+      articleToSave.saved = true;
+
+      $ajax({
+          method: "PATCH",
+          url: "/api/headlines",
+          data: articleToSave
+      })
+      .then(function(data) {
+          if (data.ok) {
+              initPage();
+          }
+      });
+  }
+
+})
